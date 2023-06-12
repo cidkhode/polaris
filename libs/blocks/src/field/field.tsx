93ac3e1941e57e839ffba7e7f82d5a785f535a4e
Cid Khode <chidanandkhode@gmail.com>
2023-06-12 10:14:54 -0400

    adding block

---
+++ libs/blocks/src/field/field.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@polaris/core'
+
+export const Field = () => {
+  return (
+    <div>
+      This is a simple field
+      <Button />
+    </div>
+  )
+}
+
+export default Field
